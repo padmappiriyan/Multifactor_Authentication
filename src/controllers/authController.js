13e60ab8dbd851db16ae93c965ed129d1d0b9c08
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-04 21:20:10 +0530

    Implement 2FA setup, verification, and reset functionality with QR code generation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 import userModel from "../models/user.js";
 import bcrypt from "bcryptjs";
+import speakeasy from "speakeasy";
+import qrCode from "qrcode";
+import jwt from "jsonwebtoken";
 
 export const register = async(req,res)=>{
         try{
@@ -93,17 +96,83 @@ export const logout = async(req,res)=>{
 }
 
 export const setup2FA =async (req,res)=>{
-
+      try{
+           console.log("The request user is ",req.user);
+           const user= req.user;
+           var secret=speakeasy.generateSecret();
+           console.log("The secret generated is ",secret);
+           user.twoFactorSecret= secret.base32;
+           user.isMfaActivate=true;
+           await user.save();
+           const url= speakeasy.otpauthURL({
+            secret:secret.base32,
+            label:`MFA-Demo(${req.user.username})`,
+            issuer:"www.piriyan.com",
+            encoding:"base32"
+      })
+      //pass this url to qrcode so that it can genrate qrcode image
+     const qrImageurl=await qrCode.toDataURL(url);
+     res.status(200).json({
+        success:true,
+        message:"2FA is successfully activated",
+        qrCode:qrImageurl,
+        secret:secret.base32
+    })
+}
+      catch(error){
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"Error in setting up 2FA"});
+      }
 }
 
 
 export const verify2FA = async(req,res)=>{
+    const {token} = req.query;
+    const user = req.user;
+    const verified= speakeasy.totp.verify({
+        secret:user.twoFactorSecret,
+        encoding:"base32",
+        token,
+})
+if(verified){
+   const jwtToken=jwt.sign({
+    username:user.username
+   },process.env.JWT_SECRET,{expiresIn:"1h"});
+
+    return res.status(200).json({   
+        success:true,
+        message:"2FA verification is successful",
+        token:jwtToken
+})
+}
+else{
+    return res.status(400).json({   
+        success:false,
+        message:"Invalid token"
+})
+}
 
 }
 
-
 export const  reset2FA= async(req,res)=>{
-
+          try{
+            const user = req.user;
+            user.isMfaActivate=false;
+            user.twoFactorSecret="";
+            await user.save();
+            res.status(200).json({
+                success:true,
+                message:"2FA is successfully reset, please setup again"
+            })
+          }
+          catch(error){
+            console.log(error);
+            res.status(500).json({
+                success:false,
+                message:"Error in resetting 2FA"});
+          }
 }
 
 
