6371fc787ff73cd449a00ef88781a8f08a104791
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-05 23:11:24 +0530

    Initialize Vite project with React and Tailwind CSS setup
    - Created package.json with dependencies and scripts for Vite, React, and Tailwind CSS.
    - Added Vite logo SVG to public directory.
    - Created App.css for Tailwind CSS imports.
    - Implemented main App component with RouterProvider and SessionProvider.
    - Added React logo SVG to assets directory.
    - Created index.css for global styles and Tailwind CSS imports.
    - Set up main entry point (main.jsx) for React application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ client/vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
+import { SessionProvider } from './context/SessionContext'
+const App = () => {
+  return (
+    <div className='bg-slate-900 h-screen'>
+      <div className='flex justify-center items-center h-screen'>
+        <SessionProvider>
+          <RouterProvider router={router} />
+        </SessionProvider>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
