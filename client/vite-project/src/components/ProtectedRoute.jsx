a99d09b7e5bc4f1100b0ad18cfc7877a4556aff8
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-06 09:49:56 +0530

    Add loading state to ProtectedRoute for improved user experience

--- client/vite-project/src/components/ProtectedRoute.jsx
+++ client/vite-project/src/components/ProtectedRoute.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Outlet ,Navigate} from 'react-router-dom';
 import { useSession } from '../context/SessionContext';
 const ProtectedRoute = () => {
-    const {isLoggedIn}=useSession(); 
+    const {isLoggedIn,loading}=useSession(); 
+    if(loading)
+        return <div>Loading...</div>
   return isLoggedIn ? <Outlet/> : <Navigate to="/login" />
 }
 
