37d93d38b0c1ff164299ce300b041e6fb65b2e0d
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-06 19:03:34 +0530

    Home page implement

--- client/vite-project/src/pages/HomePage.jsx
+++ client/vite-project/src/pages/HomePage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='text-white text-3xl'>
+      Welcome to Home Page
+
+    </div>
   )
 }
 
