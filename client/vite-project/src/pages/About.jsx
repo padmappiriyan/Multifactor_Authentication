1b52b5085981fb068b206a8ad07380a6567aaaa3
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-07 13:53:53 +0530

    create About Page

---
+++ client/vite-project/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const About = () => {
+  return (
+
+    <div className='h-screen w-full bg-white'>
+        <Navbar />
+    </div>
+  )
+}
+
+export default About
