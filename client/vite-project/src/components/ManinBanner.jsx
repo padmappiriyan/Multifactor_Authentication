950b332bc98db10c34476490c517e89b7fe8c5b0
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-08 22:08:56 +0530

    Create the MainBanner Component

---
+++ client/vite-project/src/components/ManinBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import banner from '../assets/Delivery_menu.jpg'
+
+const ManinBanner = () => {
+  return (
+    <div>
+        <img src={banner} alt='banner' className='w-full hidden md:block' />
+    </div>
+  )
+}
+
+export default ManinBanner
