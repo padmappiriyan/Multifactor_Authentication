fa88adfe5dbe142c377104e728fa9ffe335d4f34
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-08 22:09:18 +0530

    upadte the Home apge

--- client/vite-project/src/pages/HomePage.jsx
+++ client/vite-project/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import ManinBanner from '../components/ManinBanner'
 
 const HomePage = () => {
   return (
     <div className='text-white text-3xl h-screen w-full bg-white'>
-        <Navbar />
-      
+      <Navbar />
+     
 
     </div>
   )
